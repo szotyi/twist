51d2d359654b661ff81c9bf0db598f7610987011
Péter Kovács <szotyi@imind.eu>
2014-03-20 10:13:45 +0100

    Initial Commit

---
+++ Twist/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Twist
+//
+//  Created by Peter Kovacs on 20/03/14.
+//  Copyright (c) 2014 iMind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Twist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twist
+//
+//  Created by Peter Kovacs on 20/03/14.
+//  Copyright (c) 2014 iMind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
